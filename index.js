41583993c3761b0d156427c90680ca1f20d090a9
Zhao Geng <you@example.com>
2023-04-06 15:47:13 +0100

    adding an environmental variable

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000
-app.get('/', (req,res)=>res.send("Hello World"));
+
+
+app.get('/', (req,res)=>res.send("Hello World,  my web host name is " + process.env.APP_HOST_NAME || " not defined "));
+
+
 app.listen(port, ()=>console.log("server is running on port" + port))
