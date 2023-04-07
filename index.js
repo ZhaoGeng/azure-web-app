ecf6d52d151a71ceefad102726e967184060226f
Zhao Geng <you@example.com>
2023-04-07 15:53:54 +0100

    enable feature flag with Azure App Configuration

--- index.js
+++ index.js
@@ -1,9 +1,25 @@
 const express = require('express');
 const app = express();
+const { AppConfigurationClient } = require("@azure/app-configuration");
+
+require('dotenv').config()
 const port = process.env.PORT || 3000
 
 
-app.get('/', (req,res)=>res.send("Hello World,  my web host name is " + process.env.APP_HOST_NAME || " not defined "));
+async function start() {
+  let retrievedSetting = await client.getConfigurationSetting({
+    key: ".appconfig.featureflag/NEW-FEATURE"
+  });
+
+  const updatedText = retrievedSetting.value && retrievedSetting.value.enabled ? "NEW FEATURE ENABLED" : "NEW FEATURE DISABLED";
+
+  app.get('/', (req,res)=>res.send("Hello World, "+ updatedText +" my web host name is " + process.env.APP_HOST_NAME || " not defined. "));
+}
+
+// Create an AppConfigurationClient that will authenticate through AAD in the China cloud
+const client = new AppConfigurationClient(process.env.FEATURE_FLAG_CONNECTION_STRING);
 
 
+// Call the method which fetches the data
+start().catch((err) => console.log("ERROR:", err));
 app.listen(port, ()=>console.log("server is running on port" + port))
