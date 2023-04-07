b588d2f21b83c377259f021640385acfe399ffdf
Zhao Geng <you@example.com>
2023-04-07 16:17:03 +0100

    update the feature flag control

--- index.js
+++ index.js
@@ -6,20 +6,32 @@ require('dotenv').config()
 const port = process.env.PORT || 3000
 
 
-async function start() {
+async function getFeatureFlag() {
   let retrievedSetting = await client.getConfigurationSetting({
     key: ".appconfig.featureflag/NEW-FEATURE"
   });
 
-  const updatedText = retrievedSetting.value && retrievedSetting.value.enabled ? "NEW FEATURE ENABLED" : "NEW FEATURE DISABLED";
-
-  app.get('/', (req,res)=>res.send("Hello World, "+ updatedText +" my web host name is " + process.env.APP_HOST_NAME || " not defined. "));
+  return retrievedSetting.value && retrievedSetting.value ? JSON.parse(retrievedSetting.value) : {}; 
 }
 
 // Create an AppConfigurationClient that will authenticate through AAD in the China cloud
 const client = new AppConfigurationClient(process.env.FEATURE_FLAG_CONNECTION_STRING);
 
 
-// Call the method which fetches the data
-start().catch((err) => console.log("ERROR:", err));
+app.get('/', (req,res)=>{
+    // Call the method which fetches the data
+    getFeatureFlag()
+    .then((config)=>{
+       const updatedText = config.enabled ? "NEW FEATURE ENABLED" : "NEW FEATURE DISABLED";
+       return res.send("Hello World, "+ updatedText +" my web host name is " + process.env.APP_HOST_NAME || " not defined. ");
+     })
+    .catch((err) => {
+      console.log("ERROR:", err);
+      return res.send("Feature Flag Error Happened");
+    });
+});
+
+
+
+
 app.listen(port, ()=>console.log("server is running on port" + port))
